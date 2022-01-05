00730c20288c350dd9266b630d23b9d21a11a0d2
Artem Zakharchenko <kettanaito@gmail.com>
2022-01-05 02:15:25 +0100

    feat: ignore private and one-time sponsorships

--- app/routes/github/new-sponsor-tweet.ts
+++ app/routes/github/new-sponsor-tweet.ts
@@ -7,27 +7,38 @@ export const loader: LoaderFunction = () => {
 }
 
 export const action: ActionFunction = async ({ request }) => {
-  const { hook, sender } = await request.json()
-
-  if (hook.type !== '???') {
-    return new Response(`Unknown hook type "${hook.type}", doing nothing.`, {
-      status: 200,
-    })
-  }
+  const { action, hook, sponsorship } = await request.json()
 
   if (hook.config.secret !== process.env.GITHUB_WEBHOOK_SECRET) {
     return new Response('Missing or invalid webhook secret.', { status: 403 })
   }
 
+  if (action !== 'created') {
+    return new Response(`Unknown sponsorship action "${action}", ignoring.`, {
+      status: 200,
+    })
+  }
+
+  if (sponsorship !== 'public') {
+    return new Response('Ignoring a private sponsorship.')
+  }
+
+  if (sponsorship.tier.is_one_time) {
+    return new Response('Only monthly sponsors get Twitter shoutouts.')
+  }
+
   const userResponse = await githubClient.users.getByUsername({
-    username: sender.login,
+    username: sponsorship.sponsor.login,
   })
   const { data: user } = userResponse
-
   const twitterUsername = user.twitter_username
+
+  // Mention the sponsor's Twitter handle if set, otherwise use the link
+  // to their GitHub profile.
   const userMention = twitterUsername ? `@${twitterUsername}` : user.html_url
 
-  const tweetMessage = `Thank you for sponsoring us, ${user.login} (${userMention})!`
+  const tweetMessage = `Thank you for supporting us, ${user.login} (${userMention})! We are honored to have you as a new ${sponsorship.tier.name}!`
+
   const res = await twitterClient.v2.tweet(tweetMessage)
   const tweetUrl = `https://twitter.com/ApiMocking/status/${res.data.id}`
 
